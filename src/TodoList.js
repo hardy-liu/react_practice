d883078126a8f775a5325aac70dc347a0b3397ed
hardy <hardy.liu@outlook.com>
2018-12-26 13:03:12 +0800

    容器组件和ui组件相分离

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import 'antd/dist/antd.css'
-import { Button, Input, List } from 'antd'
+// import { Button, Input, List } from 'antd'
 import store from './store'
 // import ActionTypes from './store/actionTypes'
 import ActionCreator from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
   constructor (props) {
@@ -13,6 +13,7 @@ class TodoList extends Component {
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleStoreChange = this.handleStoreChange.bind(this)
     this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleListClick = this.handleListClick.bind(this)
 
     store.subscribe(this.handleStoreChange)
   }
@@ -52,26 +53,15 @@ class TodoList extends Component {
 
   render () {
     return (
-      <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <div>
-          <Input
-            value={this.state.inputValue}
-            placeholder='todo list'
-            style={{width: '300px', marginRight: '10px'}}
-            onChange={this.handleInputChange}
-          />
-          <Button type='primary' onClick={this.handleBtnClick}>提交</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (<List.Item onClick={this.handleListClick.bind(this, index)}>{item}</List.Item>)}
-        />
-      </div>
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        list={this.state.list}
+        handleInputChange={this.handleInputChange}
+        handleBtnClick={this.handleBtnClick}
+        handleListClick={this.handleListClick}
+      />
     )
   }
 }
-
 
 export default TodoList
